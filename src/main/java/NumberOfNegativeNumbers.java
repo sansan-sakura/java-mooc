56063a9e7ed0ee6ce5b206430c47a16384fd833a
sansan-sakura <sito6496t@gmail.com>
2026-07-07 16:41:09 +0200

    add number of negative numbers

---
+++ src/main/java/NumberOfNegativeNumbers.java
@@ -0,0 +1,24 @@
+package part2;
+import java.util.Scanner;
+
+public class NumberOfNegativeNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        while(true){
+            System.out.println("Give a number:");
+
+            int input = Integer.valueOf(scanner.nextLine());
+
+            if(input == 0){
+                break;
+            }
+            if(input < 0){
+                count ++;
+            }
+        }
+        System.out.println("Number of negative numbers: " + count);
+
+    }
+}
