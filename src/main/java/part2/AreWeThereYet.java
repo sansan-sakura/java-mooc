54ed2aba466260190be9d6f93e72a60ad01cdc5c
sansan-sakura <sito6496t@gmail.com>
2026-07-07 16:19:42 +0200

    add are we there yet

---
+++ src/main/java/part2/AreWeThereYet.java
@@ -0,0 +1,17 @@
+package part2;
+import java.util.Scanner;
+
+public class AreWeThereYet {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+            if(input == 4){
+                break;
+            }
+        }
+
+    }
+}
