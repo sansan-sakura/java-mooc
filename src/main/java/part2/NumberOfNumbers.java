fe285f97240366b9781fcd8c071c2bc7f863ec71
sansan-sakura <sito6496t@gmail.com>
2026-07-07 16:38:18 +0200

    add number of numbers

---
+++ src/main/java/part2/NumberOfNumbers.java
@@ -0,0 +1,23 @@
+package part2;
+import java.util.Scanner;
+
+public class NumberOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        while(true){
+            System.out.println("Give a number:");
+
+            int input = Integer.valueOf(scanner.nextLine());
+
+            if(input == 0){
+                break;
+            }
+
+            count ++;
+        }
+        System.out.println("Number of numbers: " + count);
+    }
+}
+
