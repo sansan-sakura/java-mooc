b4f659e768af4bad75863d987664bb2265ab72fe
sansan-sakura <sito6496t@gmail.com>
2026-07-07 18:12:29 +0200

    add average of numbers

---
+++ src/main/java/part2/AverageOfNumbers.java
@@ -0,0 +1,21 @@
+Scanner scanner = new Scanner(System.in);
+int sum = 0;
+int count = 0;
+
+        while(true){
+                System.out.println("Give a number:");
+int input = Integer.valueOf(scanner.nextLine());
+
+            if(input == 0){
+        break;
+        }
+
+sum = sum + input;
+count++;
+        }
+
+double average =(double) sum/count;
+        System.out.println("Average of the numbers: " + average);
+
+
+    }
