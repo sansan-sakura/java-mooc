279e21f08e243a9402975cc99fcbdcafe5050ab5
sansan-sakura <sito6496t@gmail.com>
2026-07-07 19:20:07 +0200

    add sum of sequence the sequel

---
+++ src/main/java/part2/SumOfASequenceTheSequel.java
@@ -0,0 +1,21 @@
+package part2;
+
+import java.util.Scanner;
+
+public class SumOfASequenceTheSequel {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum =0;
+        System.out.println("First number?");
+        int start = Integer.valueOf(scanner.nextLine());
+        System.out.println("Last number?");
+        int end = Integer.valueOf(scanner.nextLine());
+        for(int i =start; i<= end; i++){
+            sum=sum+i;
+        }
+        System.out.println("The sum is "+ sum);
+
+    }
+}
