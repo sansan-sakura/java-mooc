5197366493460671bca178a4e7684f1a953c6a66
sansan-sakura <sito6496t@gmail.com>
2026-07-08 15:40:17 +0200

    add from parameter to one

---
+++ src/main/java/part2/FromParameterToOne.java
@@ -0,0 +1,16 @@
+
+package part2;
+public class FromParameterToOne {
+
+    public static void main(String[] args) {
+    }
+
+    public static void printFromNumberToOne(int number){
+        int i =  number;
+        while(i>0){
+            System.out.println(i);
+            i--;
+        }
+    }
+
+}
