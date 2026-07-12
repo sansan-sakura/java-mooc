dff065dadfa41ac58cd4a24b942b27697c62e257
sansan-sakura <sito6496t@gmail.com>
2026-07-12 15:39:14 +0200

    add sum

---
+++ src/main/java/part3/Sum.java
@@ -0,0 +1,18 @@
+package part3;
+import java.util.ArrayList;
+
+public class Sum {
+
+    public static void main(String[] args) {
+        // Try your method here
+    }
+
+    public static int sum(ArrayList<Integer> numbers){
+        int sum = 0;
+        for(int num : numbers){
+            sum = sum + num;
+        }
+        return sum;
+    }
+
+}
