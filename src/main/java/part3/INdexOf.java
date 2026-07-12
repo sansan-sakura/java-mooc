8c9226a9159f85651424cab56e17fd2510a9d11a
sansan-sakura <sito6496t@gmail.com>
2026-07-12 14:40:10 +0200

    add index of

---
+++ src/main/java/part3/INdexOf.java
@@ -0,0 +1,32 @@
+package part3;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class IndexOf {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+
+            list.add(input);
+        }
+
+        System.out.println("");
+
+        // implement here finding the indices of a number
+        System.out.println("Search for?");
+        int searchNumber = Integer.valueOf(scanner.nextLine());
+        for(int i = 0; i < list.size(); i++) {
+            int number = list.get(i);
+            if (searchNumber == number) {
+                System.out.println(searchNumber + " is at index " + i);
+            }
+        }
+    }
+}
