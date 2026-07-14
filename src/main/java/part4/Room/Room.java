8990b48ceb3871fb59900c887c4d07d5774c3411
sansan-sakura <sito6496t@gmail.com>
2026-07-14 15:44:36 +0200

    add room class

---
+++ src/main/java/part4/Room/Room.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sakura
+ */
+
+
+public class Room {
+    private String code;
+    private int seats;
+
+    public Room(String classCode, int numberOfSeats){
+        this.seats = numberOfSeats;
+        this.code = classCode;
+    }
+}
