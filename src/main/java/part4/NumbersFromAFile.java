32ee6415f313b0baaaf0c7f713231039f015722e
sansan-sakura <sito6496t@gmail.com>
2026-07-16 05:51:21 +0200

    add numbers from a file

---
+++ src/main/java/part4/NumbersFromAFile.java
@@ -0,0 +1,42 @@
+
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class NumbersFromAFile {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("File? ");
+        String file = scanner.nextLine();
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        try(Scanner fileScanner = new Scanner(Paths.get(file))){
+            while(fileScanner.hasNext()){
+                list.add(Integer.valueOf(fileScanner.nextLine()));
+            }
+        } catch (Exception e){
+            System.out.println("Error: " + e);
+        }
+
+
+        System.out.print("Lower bound? ");
+        int lowerBound = Integer.valueOf(scanner.nextLine());
+        System.out.print("Upper bound? ");
+        int upperBound = Integer.valueOf(scanner.nextLine());
+
+        int count = 0;
+
+        for(int num : list){
+            if(num>=lowerBound && num <= upperBound){
+                count ++;
+            }
+        }
+
+        System.out.println("Numbers: " + count);
+
+    }
+
+}
