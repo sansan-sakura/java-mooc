2bc9e2215b96ed1866553d09354a1d53b975142c
sansan-sakura <sito6496t@gmail.com>
2026-07-16 11:37:27 +0200

    add storing records

---
+++ src/main/java/part4/StoringRecords/StroingRecords.java
@@ -0,0 +1,41 @@
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StoringRecords {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Filename:");
+        String file = scan.nextLine();
+
+        ArrayList<Person> records = readRecordsFromFile(file);
+        System.out.println("Persons: " + records.size());
+        System.out.println("Persons:");
+        for (Person person : records) {
+            System.out.println(person);
+
+        }
+    }
+
+    public static ArrayList<Person> readRecordsFromFile(String file) {
+        ArrayList<Person> persons = new ArrayList<>();
+
+        try(Scanner fileScanner = new Scanner(Paths.get(file))){
+            while(fileScanner.hasNext()){
+                String row = fileScanner.nextLine();
+
+                String[] parts = row.split(",");
+                persons.add(new Person(parts[0], Integer.valueOf(parts[1])));
+            }
+
+        } catch (Exception e){
+            System.out.println("Error: " + e);
+        }
+
+        return persons;
+
+    }
+}
