bd5975058caed2f590495f00fa1364b3703e11d5
sansan-sakura <sito6496t@gmail.com>
2026-08-17 09:44:02 +0200

    add taco box

---
+++ src/main/java/part9/TacoBoxes/CustomTacoBox.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sakura
+ */
+public class CustomTacoBox implements TacoBox {
+    private int tacos;
+
+    public CustomTacoBox(int initialTacos){
+        tacos = initialTacos;
+
+    }
+
+    public int tacosRemaining(){
+        return tacos;
+    }
+
+    public void eat(){
+        if(tacos>0){
+            tacos--;
+        }
+
+
+    }
+
+
+}
