275ab0d47d6de82595b9945b0bcfe061aa7370a6
sansan-sakura <sito6496t@gmail.com>
2026-08-26 22:04:31 +0200

    add customer

---
+++ src/main/java/part11/Customer.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sakura
+ */
+public class Customer {
+    private String name;
+    private String address;
+    private String email;
+
+    public Customer(String name, String address, String email){
+        this.email = email;
+        this.address = address;
+        this.name = name;
+
+    }
+
+
+}
