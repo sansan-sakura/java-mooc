0a0dd058fe8e7a9f9c9fd32e7610f832f8c5c5af
sansan-sakura <sito6496t@gmail.com>
2026-08-27 13:07:10 +0200

    add book and plane

---
+++ src/main/java/part11/BookAndPlane/Book.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sakura
+ */
+public class Book {
+
+    private String name;
+    private String author;
+    private int pageCount;
+
+    public Book(String name, String author, int pageCount){
+        this.author = author;
+        this.name = name;
+        this.pageCount = pageCount;
+
+    }
+
+
+}
---
+++ src/main/java/part11/BookAndPlane/Plane.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sakura
+ */
+public class Plane {
+    private String ID;
+    private String model;
+    private int yearOfIntroduction;
+
+    public Plane (String id, String model, int yearOfIntroduction){
+        this.ID = id;
+        this.model = model;
+        this.yearOfIntroduction = yearOfIntroduction;
+    }
+
+}
