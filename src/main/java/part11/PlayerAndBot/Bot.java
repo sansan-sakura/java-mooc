f39d847fbfe07b683e5fe83e5018f429d249873f
sansan-sakura <sito6496t@gmail.com>
2026-08-27 13:28:26 +0200

    add bot and player

---
+++ src/main/java/part11/PlayerAndBot/Bot.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sakura
+ */
+public class Bot extends Player {
+
+
+    public Bot(String name){
+        super(name);
+
+    }
+
+    @Override
+    public void play(){
+
+
+    }
+
+    public void addMove(String move){
+
+    }
+
+}
---
+++ src/main/java/part11/PlayerAndBot/Player.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sakura
+ */
+public class Player {
+    private String name;
+
+    public Player(String name){
+        this.name = name;
+    }
+
+
+    public void play(){
+
+    }
+
+    public void printName(){
+        System.out.println(name);
+    }
+
+}
