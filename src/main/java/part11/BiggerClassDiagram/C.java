af66567a5340bdd396e17a00a2795edf5de62dd4
sansan-sakura <sito6496t@gmail.com>
2026-08-28 08:20:35 +0200

    add bigger class diagram

---
+++ src/main/java/part11/BiggerClassDiagram/C.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author sakura
+ */
+import java.util.HashSet;
+import java.util.Set;
+
+public class C extends B implements IC  {
+    private Set<E> e;
+
+    public C() {
+        e = new HashSet<>();
+    }
+}
---
+++ src/main/java/part11/BiggerClassDiagram/E.java
@@ -0,0 +1,22 @@
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sakura
+ */
+public class E {
+    private Set<C> c;
+
+    public E() {
+        c = new HashSet<>();
+    }
+
+}
