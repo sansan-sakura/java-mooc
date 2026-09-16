58a3b3711e03cf17553a294c9b48b1bdab1f1025
sansan-sakura <sito6496t@gmail.com>
2026-09-16 19:02:20 +0200

    add numbers

---
+++ src/main/java/part12/Numbers.java
@@ -0,0 +1,17 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        System.out.println("How many random numbers should be printed?");
+        int count = Integer.valueOf(scanner.nextLine());
+        for(int i = 0; i < count; i++){
+            System.out.println(random.nextInt(11));
+        }
+    }
+
+}
