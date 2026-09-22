eadad56ce0af734755cceccd41736942a6a9cbe8
sansan-sakura <sito6496t@gmail.com>
2026-09-22 11:58:27 +0200

    add my first application

---
+++ src/main/java/part13/MyFirstApplication.java
@@ -0,0 +1,23 @@
+package myFirstApplication;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class MyFirstApplication extends Application  {
+
+
+    public static void main(String[] args) {
+        launch(MyFirstApplication.class);
+    }
+
+
+
+    @Override
+    public void start(Stage window) {
+        window.setTitle("");
+        window.show();
+    }
+
+
+}
