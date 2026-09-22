a08c87ee7f0852e1f09ebc1f45299048e189741c
sansan-sakura <sito6496t@gmail.com>
2026-09-22 12:57:05 +0200

    add Notifier

---
+++ src/main/java/part13/Notifier.java
@@ -0,0 +1,41 @@
+package notifier;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class NotifierApplication extends Application {
+
+
+    public static void main(String[] args) {
+        System.out.println(NotifierApplication.class);
+    }
+
+    @Override
+    public void start(Stage window) {
+        TextField topText = new TextField();
+        Label bottomText = new Label();
+        Button button = new Button("Update");
+
+        button.setOnAction((event) -> {
+            bottomText.setText(topText.getText());
+        });
+
+        VBox componentGroup = new VBox();
+        componentGroup.setSpacing(20);
+        componentGroup.getChildren().addAll(topText, button, bottomText);
+
+        Scene scene = new Scene(componentGroup);
+
+        window.setScene(scene);
+        window.show();
+    }
+
+}
